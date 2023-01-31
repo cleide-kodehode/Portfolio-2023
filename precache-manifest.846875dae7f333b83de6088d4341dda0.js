fc796b54dd6fd1c96f19263184a24e023c116b94
Cleide Kodehode <113598976+cleide-kodehode@users.noreply.github.com>
2023-01-31 13:41:38 +0100

    Updates

---
+++ precache-manifest.846875dae7f333b83de6088d4341dda0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "077f3d28cada45ab2af4",
+    "url": "/portfolio-2023/static/css/main.1494bd9c.chunk.css"
+  },
+  {
+    "revision": "077f3d28cada45ab2af4",
+    "url": "/portfolio-2023/static/js/main.077f3d28.chunk.js"
+  },
+  {
+    "revision": "b2e1e8503dc89ada8c6e",
+    "url": "/portfolio-2023/static/js/1.b2e1e850.chunk.js"
+  },
+  {
+    "revision": "80016f702cac78dc6117",
+    "url": "/portfolio-2023/static/js/runtime~main.80016f70.js"
+  },
+  {
+    "revision": "6279ae7acce2e2685ced74d536c8893d",
+    "url": "/portfolio-2023/static/media/camper.6279ae7a.png"
+  },
+  {
+    "revision": "1d80d9b67af23bc48e5b3e13a58e8a24",
+    "url": "/portfolio-2023/static/media/camp.1d80d9b6.png"
+  },
+  {
+    "revision": "85f27e476ffd313b05f88a3c99728e20",
+    "url": "/portfolio-2023/static/media/Drumkit.85f27e47.png"
+  },
+  {
+    "revision": "3edd3a79fd0870fe4bee274951503b18",
+    "url": "/portfolio-2023/static/media/parallax.3edd3a79.png"
+  },
+  {
+    "revision": "d8248e37b8b3677efb20e7650a4e4dec",
+    "url": "/portfolio-2023/static/media/form.d8248e37.png"
+  },
+  {
+    "revision": "2f07722d6074a1cfe7d5fa51c39d7872",
+    "url": "/portfolio-2023/static/media/christmas.2f07722d.png"
+  },
+  {
+    "revision": "ba8beb8d356846c3a334388963306118",
+    "url": "/portfolio-2023/static/media/logo..ba8beb8d.png"
+  },
+  {
+    "revision": "c986d8271a2029c71772c546c2137dbb",
+    "url": "/portfolio-2023/index.html"
+  }
+];
