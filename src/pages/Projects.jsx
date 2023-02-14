077f92e4a8174cb4da7abcc4672d32d37bd6718a
Cleide Kodehode <113598976+cleide-kodehode@users.noreply.github.com>
2023-02-14 12:10:27 +0100

    change 6

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import ProjectItem from "../components/ProjectItem";
-import Drumkit from '../img/Drumkit.png';
+
 import Parallax from '../img/parallax.png';
+import Ecommerce from '../img/ecommerce.png';
+import Drumkit from '../img/Drumkit.png';
 import Form from '../img/form.png';
 import Christmas from '../img/christmas.png';
 
 
 import '../styles/Projects.css';
 
-
 function Projects() {
   return (
     <main className="projects-main">
-      <h1> My Personal Projects </h1>
-    <div className="projectList">
+      <h1> My Projects </h1>
+    <div className="project-list">
         <ProjectItem name="Parallax Website" image={Parallax} />
-        <ProjectItem name="Form" image={Form} />
-        <ProjectItem name="Drumkit" image={Drumkit} />   
+        <ProjectItem name="ecommerce" image={Ecommerce} />  
+        <ProjectItem name="Drumkit" image={Drumkit} /> 
+        <ProjectItem name="Form" image={Form} />        
         <ProjectItem name="Christmas" image={Christmas} />   
+        
     </div>
 
     </main>
