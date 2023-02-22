726420ecb3d38ef5dc7503694a126eff7d2e96c8
Cleide Kodehode <113598976+cleide-kodehode@users.noreply.github.com>
2023-02-22 10:09:51 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio-2023/precache-manifest.f5c1ca62419c77a2e030fd77e7cdf861.js"
+  "/Portfolio-2023/precache-manifest.54b955273721bc5f3c5ca3e4f4609330.js"
 );
 
 workbox.clientsClaim();
