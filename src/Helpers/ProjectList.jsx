072c0c37e426c517a8c2bfb9d59d04386c21f1af
Cleide Kodehode <113598976+cleide-kodehode@users.noreply.github.com>
2023-02-23 11:30:21 +0100

    change 18

--- src/Helpers/ProjectList.jsx
+++ src/Helpers/ProjectList.jsx
@@ -1,9 +1,11 @@
 
 import Parallax from '../img/parallax.png';
+import Birthday from '../img/Birthday.png';
 import Ecommerce from '../img/ecommerce.png';
 import Drumkit from '../img/Drumkit.png';
 import Form from '../img/Form.png';
 import Pizzashop from "../img/pizzashop.png";
+
 
 
 import "../styles/ProjectDisplay.css";
@@ -15,35 +17,36 @@ export const ProjectList = [
         image: Parallax,
         skills: "HTML & CSS",
         live: "https://cleide-kodehode.github.io/parallax-website/",
-        github: "https://github.com/cleide-kodehode/parallax-website",
+    },
+    {
+        name: "Birthday gift site",
+        image: Birthday,
+        skills: "Html & Css",
+        live: "https://cleide-kodehode.github.io/Birthday-gift-site/",        
     },
     {
         name: "Shoes Ecommerce",
         image: Ecommerce,
         skills: "HTML, CSS, javaScript & JQuery",
         live: "https://cleide-kodehode.github.io/Shoes-ecommerce/",
-        github: "https://github.com/cleide-kodehode/Shoes-ecommerce",
     },
     {        
         name: "Drumkit",
         image: Drumkit,
         skills: "HTML, CSS & JavaScript",
         live: "https://cleide-kodehode.github.io/Drumkit/",
-        github: "https://github.com/cleide-kodehode/Drumkit",
     },
     {        
         name: "React Form",
         image: Form,
         skills: "HTML, CSS, JavaScript & React",
         live: "https://cleide-kodehode.github.io/Form-in-React/",
-        github: "https://github.com/cleide-kodehode/Form-in-React",
     },
     {
         name: "Pizza Landing Page",
         image: Pizzashop,
         skills: "Html & Css",
         live: "https://cleide-kodehode.github.io/Pizza-Landing-Page/",
-        github: "https://github.com/cleide-kodehode/Pizza-Landing-Page",
     },
 
 ];
