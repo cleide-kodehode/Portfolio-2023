ba1ec40375e7203dcca7fe8c7d1f34465c0fea97
Cleide Kodehode <113598976+cleide-kodehode@users.noreply.github.com>
2023-02-23 11:31:30 +0100

    Updates

---
+++ precache-manifest.859fabc2074466914e09ecb577f3df7e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e665796276a0c66bc14",
+    "url": "/Portfolio-2023/static/css/main.a9215d55.chunk.css"
+  },
+  {
+    "revision": "8e665796276a0c66bc14",
+    "url": "/Portfolio-2023/static/js/main.8e665796.chunk.js"
+  },
+  {
+    "revision": "2f167a31ba087faa5957",
+    "url": "/Portfolio-2023/static/js/1.2f167a31.chunk.js"
+  },
+  {
+    "revision": "19a01b20c1c9518a3a8f",
+    "url": "/Portfolio-2023/static/js/runtime~main.19a01b20.js"
+  },
+  {
+    "revision": "581d88e4a3cb2df7691143490a359a79",
+    "url": "/Portfolio-2023/static/media/camper.581d88e4.png"
+  },
+  {
+    "revision": "98450a32e1f7ca225fd4388674510f00",
+    "url": "/Portfolio-2023/static/media/html.98450a32.svg"
+  },
+  {
+    "revision": "06bbc27423cf5cdddbc3aef2d3be916e",
+    "url": "/Portfolio-2023/static/media/css.06bbc274.svg"
+  },
+  {
+    "revision": "c85c484a13d9043440f7287dba037ecf",
+    "url": "/Portfolio-2023/static/media/js.c85c484a.svg"
+  },
+  {
+    "revision": "33d467af2db1d3c036d59ac285ecdee6",
+    "url": "/Portfolio-2023/static/media/reacticon.33d467af.svg"
+  },
+  {
+    "revision": "1159fe4012ebfc30910d1eba20d09536",
+    "url": "/Portfolio-2023/static/media/figma.1159fe40.svg"
+  },
+  {
+    "revision": "90928971fd5a702260ff1319f5346cc4",
+    "url": "/Portfolio-2023/static/media/Canva.90928971.svg"
+  },
+  {
+    "revision": "4e9aed057cdb9d4c1b0025ccc0c756da",
+    "url": "/Portfolio-2023/static/media/Github.4e9aed05.svg"
+  },
+  {
+    "revision": "da0f8a74dcdf22f6abab97b8a6910b0f",
+    "url": "/Portfolio-2023/static/media/Git.da0f8a74.svg"
+  },
+  {
+    "revision": "3edd3a79fd0870fe4bee274951503b18",
+    "url": "/Portfolio-2023/static/media/parallax.3edd3a79.png"
+  },
+  {
+    "revision": "db2dfae25666c1529aa7d08955a8e5d9",
+    "url": "/Portfolio-2023/static/media/Birthday.db2dfae2.png"
+  },
+  {
+    "revision": "3e30ec9bb74f60382cdfebf96199a628",
+    "url": "/Portfolio-2023/static/media/ecommerce.3e30ec9b.png"
+  },
+  {
+    "revision": "0482f780bdaa5e3f315ee910a1136054",
+    "url": "/Portfolio-2023/static/media/Drumkit.0482f780.png"
+  },
+  {
+    "revision": "c4d392d0f8cb1b4a733f16025cd2d8cd",
+    "url": "/Portfolio-2023/static/media/Form.c4d392d0.png"
+  },
+  {
+    "revision": "c0fa683ede551b4712a4a62a98eaf5b9",
+    "url": "/Portfolio-2023/static/media/pizzashop.c0fa683e.png"
+  },
+  {
+    "revision": "ba8beb8d356846c3a334388963306118",
+    "url": "/Portfolio-2023/static/media/logo..ba8beb8d.png"
+  },
+  {
+    "revision": "e22a55fdf5baa6505161598d704ad968",
+    "url": "/Portfolio-2023/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio-2023/precache-manifest.e104869dce5851240ba2475fbe9eb69e.js"
+  "/Portfolio-2023/precache-manifest.859fabc2074466914e09ecb577f3df7e.js"
 );
 
 workbox.clientsClaim();
