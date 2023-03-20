04737f608dc6552c3a5a1921041c969266d4986b
Cleide Kodehode <113598976+cleide-kodehode@users.noreply.github.com>
2023-03-20 14:16:27 +0100

    commit changes in the hamburger dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,6 +29,15 @@ function Navbar() {
         <img src={Logo} alt="project logo" onClick={goHome} />
       </div>
 
+      <div>
+        <ul className="hamburguer-dropdown-menu">
+          <li><a href="home">Home</a></li>
+          <li><a href="about">About</a></li>
+          <li><a href="projects">Projects</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+      </div>  
+
       <nav className="links">
         <Link to= "/"> Home </Link> 
         <Link to= "/about"> About </Link> 
@@ -42,7 +51,7 @@ function Navbar() {
           }}>
           <MdOutlineReorder />
           </button>
-      </div>  
+        </div>  
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Camper from "../img/camper.png";
 function Home() {
   return (
     <main className="home-main">
-      <h2> Hey there! <span role="img" aria-label="Grinning Face"> &#128512;</span> </h2>
+      <h2> Hey there!<span role="img" aria-label="Grinning Face"> &#128512;</span> </h2>
       <img src= {Camper} className="home-img"  alt="solo female camper" />
       <h1> I'm Cleide Florisbella</h1>
       <p> Passionate about photography, webdesign & country music...   
