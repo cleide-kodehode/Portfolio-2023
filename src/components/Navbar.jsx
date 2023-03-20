ff0721f5512a4a0da9467f7794ec567f56c765aa
Cleide Kodehode <113598976+cleide-kodehode@users.noreply.github.com>
2023-03-20 14:47:52 +0100

    commit changes harburger hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ function Navbar() {
       </div>
 
       <div>
-        <ul className="hamburguer-dropdown-menu">
+        <ul className="hamburger-dropdown-menu">
           <li><a href="home">Home</a></li>
           <li><a href="about">About</a></li>
           <li><a href="projects">Projects</a></li>
